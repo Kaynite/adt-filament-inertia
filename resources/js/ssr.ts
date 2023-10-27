244d55b1e10498d8f2489f655e5191da6a98b8fd
Kareem <Kudo.200200@gmail.com>
2023-10-28 01:34:09 +0200

    TS Config an SSR Fixing

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -5,6 +5,7 @@ import createServer from '@inertiajs/vue3/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { Link, Head } from "@inertiajs/vue3"
+import VueSplide from '@splidejs/vue-splide';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -17,6 +18,7 @@ createServer((page) =>
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
+                .use(VueSplide)
                 .component("Head", Head)
                 .component("Link", Link)
                 .use(ZiggyVue, {
